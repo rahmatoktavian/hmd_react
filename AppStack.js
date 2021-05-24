5cd23f095355d5d6c0e60d018b674129d6ca0813
rahmatoktavian <rahmatoktavian@gmail.com>
2021-05-24 10:47:48 +0700

    initapp

---
+++ AppStack.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+import HomeScreen from './screen_nav/HomeScreen';
+import ProfileScreen from './screen_nav/ProfileScreen';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
