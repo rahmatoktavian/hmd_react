69f49a63e5a3712e8eae3761dc6d655c5a4e3ded
rahmatoktavian <rahmatoktavian@gmail.com>
2021-08-24 13:50:48 +0700

    initapp

--- screen_stack/HomeScreen.js
+++ screen_stack/HomeScreen.js
@@ -6,20 +6,25 @@ class HomeScreen extends Component {
   constructor(props) {
       super(props);
 
+      //default state
       this.state = {
-        ...this.state,
         nama: '',
         gender: '',
         anak: [],
       };
   }
 
+  //komponen yg pertama di load
   componentDidMount() {
+
+    //let : variable bisa diubah
     let nama = 'Rahmat';
     nama = 'Mamat';
 
+    //const : constanta yg ga bisa diubah
     const gender = 'Pria';
 
+    //format array
     let anak = [
                 {nama:'Jihan', umur:7},
                 {nama:'Hamidah', umur:3},
@@ -32,6 +37,7 @@ class HomeScreen extends Component {
       return (
         <View>
           <Text>Home Screen</Text>
+          <Text>{this.state.nama}</Text>
           <Button
             title="Go to Profile"
             onPress={() => this.props.navigation.navigate('ProfileScreen', {nama:this.state.nama, gender:this.state.gender, listAnak:this.state.anak})}
